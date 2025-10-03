527dc073b7eb7dd3292f74b6caff6ba807dada2b
Diego Nova <dnovar@ulasalle.edu.pe>
2025-10-02 23:43:57 -0500

    Agregando programa compartiendo mesa

---
+++ AsiloLiraWeb/AsiloLiraWeb/AsiloLiraWeb/Controllers/Programas.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace AsiloLiraWeb.Controllers
+{
+    public class Programas : Controller
+    {
+        public IActionResult CuidandoCompartiendo()
+        {
+            return View();
+        }
+    }
+}

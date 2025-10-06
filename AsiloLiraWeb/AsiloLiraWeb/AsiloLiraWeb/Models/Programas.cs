050bca3e9b48f246e25b73ac2620a340c878cf30
Diego Nova <dnovar@ulasalle.edu.pe>
2025-10-06 18:21:13 -0500

    Agregando modelo Programas y sus atributos

---
+++ AsiloLiraWeb/AsiloLiraWeb/AsiloLiraWeb/Models/Programas.cs
@@ -0,0 +1,15 @@
+﻿namespace AsiloLiraWeb.Models
+{
+    public class Programas
+    {
+        public int Id { get; set; }
+        public string Nombre { get; set; } 
+        public string Descripcion1 { get; set; } 
+        public string Descripcion2 { get; set; }
+        public string Resumen { get; set; }
+        public string Habilidad1 { get; set; }
+        public string Habilidad2 { get; set; }
+        public string Habilidad3 { get; set; }
+        public string Imagen { get; set; }
+    }
+}

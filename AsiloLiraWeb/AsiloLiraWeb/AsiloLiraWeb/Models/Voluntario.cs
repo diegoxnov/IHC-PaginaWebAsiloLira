b222098e070236d3299758d429ef3008b39bded3
Diego Nova <diegonova368@gmail.com>
2025-11-12 09:53:12 -0500

    Agregando correcciones de interfaz

--- AsiloLiraWeb/AsiloLiraWeb/AsiloLiraWeb/Models/Voluntario.cs
+++ AsiloLiraWeb/AsiloLiraWeb/AsiloLiraWeb/Models/Voluntario.cs
@@ -21,9 +21,9 @@ namespace AsiloLiraWeb.Models
         public string Correo { get; set; }
         public string EntidadEducativa { get; set; }
         public string Carrera { get; set; }
-        public string Semestre { get; set; }
-        public string Ciudad { get; set; }
-        public string Distrito { get; set; }
+       // public string Semestre { get; set; }
+       // public string Ciudad { get; set; }
+       // public string Distrito { get; set; }
 
     }
 }

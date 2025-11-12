374e1f59367a5edb808dbf159107c44ab62a2176
Diego Nova <diegonova368@gmail.com>
2025-11-12 09:05:48 -0500

    Corrigiendo mejora de interfaz IndexHome, boton y tamaño de video

--- AsiloLiraWeb/AsiloLiraWeb/AsiloLiraWeb/Models/Voluntario.cs
+++ AsiloLiraWeb/AsiloLiraWeb/AsiloLiraWeb/Models/Voluntario.cs
@@ -15,6 +15,9 @@ namespace AsiloLiraWeb.Models
         public string Edad { get; set; }
         [Required(ErrorMessage = "El campo {0} es requerido")]
         public string Telefono { get; set; }
+        [Required(ErrorMessage = "El campo {0} es requerido")]
+        [RegularExpression(@"^\d{8}$", ErrorMessage = "El campo {0} debe tener exactamente 8 dígitos")]
+        public string DNI { get; set; }
         public string Correo { get; set; }
         public string EntidadEducativa { get; set; }
         public string Carrera { get; set; }
